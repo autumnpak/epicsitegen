180dbf43e8703a543fb57e3055f2c6d16205648d
Autumn Pak <pak.autumn@gmail.com>
2024-05-11 12:10:48 +1000

    file io caching attempt

---
+++ src/io.rs
@@ -0,0 +1,38 @@
+use std::collections::HashMap;
+use std::collections::hash_map::Entry;
+use std::path::Path;
+use std::fs;
+
+pub enum FileError {
+    FileNotFound(String),
+    FileCantBeRead(String),
+}
+
+trait ReadsFiles {
+    fn read(&mut self, filename: &str) -> Result<&str, FileError>;
+}
+
+pub struct FileCache {
+    files: HashMap<String, String>,
+    file_pipes: HashMap<(String, Vec<String>), String>,
+}
+
+fn read_file(filename: &str) -> Result<String, FileError> {
+    if Path::exists(Path::new(filename)) {
+        match fs::read_to_string(filename) {
+            Ok(strr) => Ok(strr),
+            Err(_) => Err(FileError::FileCantBeRead(filename.to_owned())),
+        }
+    } else {
+        Err(FileError::FileNotFound(filename.to_owned()))
+    }
+}
+
+impl ReadsFiles for FileCache {
+    fn read(&mut self, filename: &str) -> Result<&str, FileError> {
+        Ok(match self.files.entry(filename.to_owned()) {
+            Entry::Occupied(ee) => ee.into_mut(),
+            Entry::Vacant(ee) => ee.insert(read_file(filename)?),
+        })
+    }
+}
--- src/lib.rs
+++ src/lib.rs
@@ -1,6 +1,7 @@
 pub mod template;
 pub mod parsers;
 pub mod yaml;
+pub mod io;
 
 fn main() {
     println!("Hello, world!");
